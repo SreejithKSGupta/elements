bc78bd7c0cf7d686561aef72a58efb69304f766b
sreejith <sreejithksgupta2255@gmail.com>
2024-08-18 09:28:01 +0530

    added SEO

---
+++ src/routes/Seo.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { Seo } from '@svelteuidev/core';
+  
+    const title = 'Period';
+    const description = 'Explore the periodic table with interactive features, including 3D Bohr views of atoms and detailed element data.';
+    const imageUrl = '/favicon.webp';
+    const siteUrl = 'https://elements-bmeh.vercel.app/'; 
+  </script>
+  
+  <Seo
+    title={title}
+    titleTemplate="%t% | Sreejith's Periodic Table"
+    description={description}
+    canonical={siteUrl}
+    openGraph={{
+      url: siteUrl,
+      type: 'website',
+      title,
+      description,
+      images: [
+        {
+          url: imageUrl,
+          alt: 'Periodic Table Project',
+          width: 1200,
+          height: 630,
+        },
+      ],
+      site_name: "Sreejith's Periodic Table",
+    }}
+    twitter={{
+      cardType: 'summary_large_image',
+      title,
+      description,
+      image: imageUrl,
+      imageAlt: 'Periodic Table Project',
+    }}
+  />
+  
---
+++ todo.md
@@ -0,0 +1,4 @@
+1. sharable links for elements
+2. Better UI and structure
+3. better error handling
+4. Add series (alkaline series, electronegativity series etc)
